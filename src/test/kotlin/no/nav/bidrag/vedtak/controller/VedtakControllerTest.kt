a76321e02b1f03e18c6acd3579b7c7d4219e4329
rinnan17 <magnus.rinnan@nav.no>
2021-12-03 12:21:00 +0100

    fjernet bidrag-vedtak fra path

--- src/test/kotlin/no/nav/bidrag/vedtak/controller/VedtakControllerTest.kt
+++ src/test/kotlin/no/nav/bidrag/vedtak/controller/VedtakControllerTest.kt
@@ -84,8 +84,8 @@ class VedtakControllerTest {
   @LocalServerPort
   private val port = 0
 
-  @Value("\${server.servlet.context-path}")
-  private val contextPath: String? = null
+/*  @Value("\${server.servlet.context-path}")
+  private val contextPath: String? = null*/
 
   private val vedtakDtoListe = object : ParameterizedTypeReference<List<VedtakDto>>() {}
 
@@ -201,7 +201,7 @@ class VedtakControllerTest {
   }
 
   private fun makeFullContextPath(): String {
-    return "http://localhost:$port$contextPath"
+    return "http://localhost:$port"
   }
 
   private fun byggRequest(): HttpEntity<OpprettVedtakRequest> {
