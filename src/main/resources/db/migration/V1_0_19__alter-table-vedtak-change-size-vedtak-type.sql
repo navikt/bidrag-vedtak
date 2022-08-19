ba05757c668cb519a4d3540d63e298abed90c65d
rinnan17 <magnus.rinnan@nav.no>
2022-08-19 14:29:57 +0200

    Retter feil flywayscript

--- src/main/resources/db/migration/V1_0_19__alter-table-vedtak-change-size-vedtak-type.sql
+++ src/main/resources/db/migration/V1_0_19__alter-table-vedtak-change-size-vedtak-type.sql
@@ -1,2 +1,4 @@
 ALTER TABLE vedtak
-    ALTER COLUMN vedtak_type varchar(50) NOT NULL default 'MANUELT' ;
+    ALTER COLUMN vedtak_type TYPE varchar(50);
+ALTER TABLE vedtak
+    ALTER COLUMN vedtak_type SET default 'MANUELT' ;
