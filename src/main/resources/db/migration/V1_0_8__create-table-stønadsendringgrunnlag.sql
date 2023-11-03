b1f1f681ccba485cb48193213395e3d047c28a63
Magnus Rinnan <magnus.rinnan@nav.no>
2023-11-03 16:13:29 +0100

    Fikser flywayfeil

--- src/main/resources/db/migration/V1_0_8__create-table-stønadsendringgrunnlag.sql
+++ src/main/resources/db/migration/V1_0_8__create-table-stønadsendringgrunnlag.sql
@@ -4,22 +4,22 @@
 
 CREATE TABLE IF NOT EXISTS stønadsendringgrunnlag
 (
-    stønadsendringid integer NOT NULL,
+    stønadsendringsid integer NOT NULL,
     grunnlagsid integer NOT NULL,
-    CONSTRAINT stønadsendringgrunnlag_pkey PRIMARY KEY (stønadsendringid, grunnlagsid),
-    CONSTRAINT fk_stønadsendring_id FOREIGN KEY (stønadsendringid)
-        REFERENCES stønadsendring (stønadsendringid) MATCH SIMPLE
+    CONSTRAINT stønadsendringgrunnlag_pkey PRIMARY KEY (stønadsendringsid, grunnlagsid),
+    CONSTRAINT fk_stønadsendring_id FOREIGN KEY (stønadsendringsid)
+        REFERENCES stønadsendring (stønadsendringsid) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
     CONSTRAINT fk_grunnlag_id FOREIGN KEY (grunnlagsid)
         REFERENCES grunnlag (grunnlagsid) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    UNIQUE (stønadsendringid, grunnlagsid)
+    UNIQUE (stønadsendringsid, grunnlagsid)
 )
 
     TABLESPACE pg_default;
 
-CREATE INDEX idx_stønadsendringgrunnlag_1 ON stønadsendringgrunnlag(stønadsendringid);
+CREATE INDEX idx_stønadsendringgrunnlag_1 ON stønadsendringgrunnlag(stønadsendringsid);
 
-CREATE INDEX idx_stønadsendringgrunnlag_2 ON stønadsendringgrunnlag(stønadsendringid, grunnlagsid);
+CREATE INDEX idx_stønadsendringgrunnlag_2 ON stønadsendringgrunnlag(stønadsendringsid, grunnlagsid);
