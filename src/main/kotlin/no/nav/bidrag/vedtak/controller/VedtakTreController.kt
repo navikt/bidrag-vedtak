52de00461639b8dcc455205ad3532520c48d26ae
Ugur Cenar <cenar93@gmail.com>
2024-02-20 16:08:50 +0100

    Legg til vedtak tree map generator

---
+++ src/main/kotlin/no/nav/bidrag/vedtak/controller/VedtakTreController.kt
@@ -0,0 +1,34 @@
+package no.nav.bidrag.vedtak.controller
+
+import io.micrometer.core.annotation.Timed
+import no.nav.bidrag.vedtak.service.TreeChild
+import no.nav.bidrag.vedtak.service.VedtakService
+import no.nav.bidrag.vedtak.service.toMermaid
+import no.nav.bidrag.vedtak.service.toTree
+import no.nav.security.token.support.core.api.Protected
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@Protected
+@Timed
+class VedtakTreController(private val vedtakService: VedtakService) {
+
+    @Suppress("unused")
+    @PostMapping("/vedtak/mermaid/{vedtakId}")
+    fun vedtakTilMermaid(@PathVariable vedtakId: Int): ResponseEntity<String> {
+        val vedtak = vedtakService.hentVedtak(vedtakId)
+        return ResponseEntity.ok().contentType(MediaType.valueOf("text/plain;charset=UTF-8"))
+            .body(vedtak.toMermaid().joinToString(""))
+    }
+
+    @Suppress("unused")
+    @PostMapping("/vedtak/graph/{vedtakId}")
+    fun vedtakTilTre(@PathVariable vedtakId: Int): ResponseEntity<TreeChild> {
+        val vedtak = vedtakService.hentVedtak(vedtakId)
+        return ResponseEntity.ok(vedtak.toTree())
+    }
+}
