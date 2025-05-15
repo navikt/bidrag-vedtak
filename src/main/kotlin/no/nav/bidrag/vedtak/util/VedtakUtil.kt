6fce8964f9902207a208cacc13855c8d7b321fb0
Ugur Alpay Cenar <cenar93@gmail.com>
2025-05-15 15:56:27 +0200

    Json converter

--- src/main/kotlin/no/nav/bidrag/vedtak/util/VedtakUtil.kt
+++ src/main/kotlin/no/nav/bidrag/vedtak/util/VedtakUtil.kt
@@ -1,20 +1,10 @@
 package no.nav.bidrag.vedtak.util
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import java.text.SimpleDateFormat
+import no.nav.bidrag.transport.felles.commonObjectmapper
 
 open class VedtakUtil {
 
     companion object {
-        fun tilJson(json: Any): String {
-            val objectMapper = ObjectMapper()
-            objectMapper.registerKotlinModule()
-            objectMapper.writerWithDefaultPrettyPrinter()
-            objectMapper.registerModule(JavaTimeModule())
-            objectMapper.dateFormat = SimpleDateFormat("yyyy-MM-dd")
-            return objectMapper.writeValueAsString(json)
-        }
+        fun tilJson(json: Any): String = commonObjectmapper.writeValueAsString(json)
     }
 }
